1fc28fd5eebb01559015b27954982f5a4698e1f9
Twinemukama Innocent <90179491+Twinemukama@users.noreply.github.com>
2023-07-13 09:46:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+// let hands=["rock", "paper", "scissor"]
+
+// function random(){
+//     let randomIndex=Math.floor(Math.random()*3)
+//     return hands[randomIndex]
+// }
+// console.log(random())
+// let fruit=["apple","orange","apple","apple","orange"]
+
+// let appleShelf=document.getElementById("apple-shelf")
+// let orangeShelf=document.getElementById("orange-shelf")
+// function sortFruit(){
+//     for(let i=0;i<fruit.length;i++){
+//         if(fruit[i]==="apple"){
+//             appleShelf.textContent+=" apple "
+//         }else {
+//             orangeShelf.textContent+=" orange "
+//         }
+//     }
+// }
+// sortFruit()
+
+// let largestCountries=["China", "India", "USA", "Indonesia", "Pakistan"]
+// console.log("The 5 largest countries in the world:")
+// for(let i=0;i<largestCountries.length;i++){
+//     console.log("- "+largestCountries[i])
+// }
+
+// let largeCountry=["Tuvalu", "India","USA","Indonesia","Monaco"]
+// largeCountry.pop()
+// largeCountry.push("Pakistan")
+// largeCountry.shift()
+// largeCountry.unshift("China")
+
+// console.log(largeCountry)
+
+// let dayOfMonth=13
+// let weekDay="Friday"
+
+// if(dayOfMonth===13 && weekDay==="Friday"){
+//     console.log("spooky face")
+// }
+// let person={
+//     name:"Innocent",
+//     age:17,
+//     country:"Uganda"
+// }
+
+// function logData(){
+//     console.log(person.name+" is "+person.age+" years old and lives in "+person.country)
+// }
+// logData()
+
+//let age=17
+
+// if(age<6){
+//   console.log("Free")
+// }
+// // age<6 already accounted for 
+// else if(age<17){
+//   console.log("Child discount")
+// }else {
+//   console.log("full amount")
+// }
+
+
+// let hasSolvedChallenge=false
+// let hasHintsLeft=false
+// if (hasSolvedChallenge===false|| hasHintsLeft===true){
+//   showSolution()
+// }
+// function showSolution(){
+//   console.log('Showing the solution')
+// }
+// let age =101
+
+// if(age<100){
+//     console.log("Not eligible")
+// }
+// else if(age===100){
+//     console.log("Here is your birthday card from the King!")
+// }
+// else{
+//     console.log("Not eligible, you have already got one")
+// }
+//creating and adding to an array
+// let messages=[
+//     "Hey, how's it going?",
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portifolio lately."
+// ]
+
+// let newMsg="Same here!"
+
+// messages.push(newMsg)
+
+
+// console.log(messages)
+//array
+// let cards1 =[7,8,9]
+
+// for (let i=0; i<cards1.length; i++){
+//     console.log(cards1[i])
+// }
+
+//getting a random number
+// let player1Time=102
+// let player2Time=107
+
+// function totalRaceTime(){
+//   return player1Time+player2Time
+// }
+// let totalTime=totalRaceTime()
+// console.log(totalTime)
+
+// function rollDice(){
+//   let randomNumber=Math.floor(Math.random()*6)+1
+//   return randomNumber
+// }
+
+ 
+//  console.log(rollDice())
